450f29f5d8431873d55ac2e77469239d957eef16
Oleksandr Lysenko <oleksandr.lysenko.jr@gmail.com>
2019-11-28 11:14:05 +0200

    Add my FIRST telegram bot on Ruby

--- Projects/telegram_bots/first_bot/main.rb
+++ Projects/telegram_bots/first_bot/main.rb
@@ -1,1 +1,21 @@
 # coding: UTF-8
+require 'telegram/bot'
+
+TOKEN = '891522345:AAHYdqQGEZWvC1xY8fiklXYvyMf8WGhGvkM'
+
+ANSWERS = ["Yes","No"]
+
+Telegram::Bot::Client.run(TOKEN) do |bot|
+  bot.listen do |message|
+    case message.text
+    when '/start' , '/start start'
+      bot.api.send_message(
+        chat_id: message.chat.id,
+        text: "Hello, my dear, #{message.from.first_name}")
+    else
+      bot.api.send_message(
+        chat_id: message.chat.id,
+        text: ANSWERS.sample)
+    end
+  end
+end
