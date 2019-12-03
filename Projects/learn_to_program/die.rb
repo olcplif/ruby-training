855441b221daae8c9e1a1c945e42a448e017e536
Oleksandr Lysenko <57721223+olcplif@users.noreply.github.com>
2019-12-03 21:46:17 +0200

    Update die.rb

--- Projects/learn_to_program/die.rb
+++ Projects/learn_to_program/die.rb
@@ -11,15 +11,27 @@ class Die #Гральна кисть
    @numberShowing
  end
 
+ # def cheat
+ #   print "Введіть бажану кість: "
+ #   numberCheat = gets.chomp.to_i
+ #   if (numberCheat > 0) and (numberCheat < 7)
+ #     @numberShowing = numberCheat
+ #   else
+ #     "Введені дані не використовується!\n Повторіть спробу."
+ #   end
+ # end
  def cheat
    print "Введіть бажану кість: "
    numberCheat = gets.chomp.to_i
-   if (numberCheat > 0) and (numberCheat < 7)
-     @numberShowing = numberCheat
-   else
-     puts "Введені дані не використовується!\n Повторіть спробу."
+   while (numberCheat != 1) #or (numberCheat != 2) or (numberCheat != 3) or (numberCheat != 4) or (numberCheat != 5) or (numberCheat != 6)
+     if (numberCheat > 0) and (numberCheat < 7)
+       @numberShowing = numberCheat
+     else
+       "Введені дані не використовується!\n Повторіть спробу."
+     end
    end
  end
+
 end
 
 # puts Die.new.showing
