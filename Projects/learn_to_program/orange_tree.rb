a94f40af3f7ac878510954cf490452de04005526
Oleksandr Lysenko <57721223+olcplif@users.noreply.github.com>
2019-12-05 00:05:51 +0200

    Edit OrangeTree - Done!

--- Projects/learn_to_program/orange_tree.rb
+++ Projects/learn_to_program/orange_tree.rb
@@ -2,56 +2,47 @@
 class OrangeTree
 
   def initialize
-    @age = 0
-    @height = 0
-    @fruiting = false
-    @orangeCount = 0
+    @age = 0 # вік дерева
+    @height = 0 # висота дерева
+    @fruiting = false # чи плодоносить дерево
+    @orangeCount = 0 # Кількість апельсинів, які вродили
     puts 'Почало рости нове Апельсинове дерево!'
   end
 
   def height
-    puts 'Тепер дерево має висоту ' + @height.to_s + ' метри'
+    puts 'Тепер дерево має висоту ' + @height.to_s + ' метри.'
   end
 
   def oneYearPasses
     @orangeCount = 0
-    @age += 1
+    puts 'Минув ' + (@age += 1).to_s + ' рік.'
     @height += 1
-    if @age > 20
+    if @age >= 3
+      @orangeCount += (@age * rand(2..4))
+    end
+    if @age > 19 # Коли дереву більше 20 років воно всихає
       puts 'Нажаль, дерево зістарілося і всохло...'
       exit
     end
-    if @age >= 3
-      @fruiting = true
-      countTheOranges
-    end
-    puts ''
   end
 
   def countTheOranges # Рахує кількість апельсинів
-    if @fruiting == true # Можливо ця умова зайва
-      @orangeCount += (2 * @age)
-      puts 'Цього року на дереві виросло ' + @orangeCount.to_s + '  апельсинів!'
+      puts 'Тепер на дереві росте ' + @orangeCount.to_s + ' апельсинів.'
+  end
+
+  def pickAnOrange # Збирання апельсинів
+    if @orangeCount > 0
+      @orangeCount -= 1
+      puts 'Цей зірваний апельсин був дуже смачний!'
+    else
+      puts 'Апельсинів для збору цього року немає.'
     end
   end
 
-  def pickAnOrange
-    @orangeCount -= 1
-    if @orangeCount > 0
-      puts 'Цей зірваний апельсин був дуже смачний!'
-    else
-      puts 'Цього року, нажаль, вже всі апельсини зібрані'
-  end
-
-end
 end
 
 orangeTree = OrangeTree.new
 orangeTree.height
-orangeTree.oneYearPasses
-orangeTree.height
-orangeTree.countTheOranges
-orangeTree.pickAnOrange
 orangeTree.countTheOranges
 orangeTree.oneYearPasses
 orangeTree.height
@@ -63,3 +54,55 @@ orangeTree.height
 orangeTree.countTheOranges
 orangeTree.pickAnOrange
 orangeTree.countTheOranges
+orangeTree.oneYearPasses
+orangeTree.height
+orangeTree.countTheOranges
+orangeTree.pickAnOrange
+orangeTree.countTheOranges
+orangeTree.pickAnOrange
+orangeTree.pickAnOrange
+orangeTree.countTheOranges
+orangeTree.pickAnOrange
+orangeTree.pickAnOrange
+orangeTree.countTheOranges
+orangeTree.pickAnOrange
+orangeTree.pickAnOrange
+orangeTree.countTheOranges
+orangeTree.oneYearPasses
+orangeTree.countTheOranges
+orangeTree.height
+orangeTree.oneYearPasses
+orangeTree.oneYearPasses
+orangeTree.oneYearPasses
+orangeTree.height
+orangeTree.oneYearPasses
+orangeTree.oneYearPasses
+orangeTree.oneYearPasses
+orangeTree.countTheOranges
+orangeTree.pickAnOrange
+orangeTree.pickAnOrange
+orangeTree.pickAnOrange
+orangeTree.pickAnOrange
+orangeTree.pickAnOrange
+orangeTree.pickAnOrange
+orangeTree.countTheOranges
+orangeTree.oneYearPasses
+orangeTree.countTheOranges
+orangeTree.oneYearPasses
+orangeTree.oneYearPasses
+orangeTree.oneYearPasses
+orangeTree.countTheOranges
+orangeTree.oneYearPasses
+orangeTree.oneYearPasses
+orangeTree.oneYearPasses
+orangeTree.oneYearPasses
+orangeTree.oneYearPasses
+orangeTree.oneYearPasses
+orangeTree.oneYearPasses
+orangeTree.oneYearPasses
+orangeTree.oneYearPasses
+orangeTree.oneYearPasses
+orangeTree.oneYearPasses
+orangeTree.oneYearPasses
+orangeTree.oneYearPasses
+orangeTree.oneYearPasses
