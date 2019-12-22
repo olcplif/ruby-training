2b3c135e239e672e629293525a0a998e2a7c6094
Oleksandr Lysenko <57721223+olcplif@users.noreply.github.com>
2019-12-22 18:48:36 +0200

    Update Like - There are a few more ways to do this

--- Projects/learn_to_program/codewars/likes.rb
+++ Projects/learn_to_program/codewars/likes.rb
@@ -10,6 +10,33 @@ def likes(names)
      else
      p 'no one likes this'
     end
+
+  #   case names.size
+  # when 0
+  #   "no one likes this"
+  # when 1
+  #   "#{names[0]} likes this"
+  # when 2
+  #   "#{names[0]} and #{names[1]} like this"
+  # when 3
+  #   "#{names[0]}, #{names[1]} and #{names[2]} like this"
+  # else
+  #   "#{names[0]}, #{names[1]} and #{names.size - 2} others like this"
+  # end
+
+#   case names.length
+#   when 0
+#     "no one likes this"
+#   when 1
+#     "%s likes this" % names
+#   when 2
+#     "%s and %s like this" % names
+#   when 3
+#     "%s, %s and %s like this" % names
+#   else
+#     "%s, %s and %d others like this" %
+#       [names[0], names[1], names.length - 2]
+#   end
 end
 
 likes([]) #, 'no one likes this')
